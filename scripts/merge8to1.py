883c2fd5aa69819b882ebf42b8eda5a095c545b4
Steven Wu <stevenhwu@gmail.com>
2017-01-30 13:19:02 -0700

    update python

--- scripts/merge8to1.py
+++ scripts/merge8to1.py
@@ -1,8 +1,8 @@
-in_name = "bwa_2.tad"
-out_name = "bwa_merged.tad"
+in_name = "bwa_filtered.tad"
+out_name = "bwa_filtered_merged.tad"
+sum_file = "bwa_filtered_sum.tad"
 
-
-with open(in_name, "r") as infile,  open(out_name, "w") as outfile:
+with open(in_name, "r") as infile,  open(out_name, "w") as outfile, open(sum_file, "w") as sumfile:
     for line in infile:
         if line.startswith("@"):
             outfile.write(line);
@@ -15,12 +15,17 @@ with open(in_name, "r") as infile,  open(out_name, "w") as outfile:
             if len(split[2])==1:
                 counts = map(int, split[3:])
                 total = str(sum(counts))
+                sum_count = total
             else:
                 counts = [ map(int, c.split(",")) for c in split[3:] ]
                 each_total = [0]*len(split[2])
                 for i in counts:
                     each_total =  [x+y for x,y in zip(each_total, i)]
                 total = ",".join(map(str,each_total))
+                sum_count = sum(each_total)
                 
             new_line = "\t".join(split[0:3]) + "\t" + total + "\n"
             outfile.write(new_line)
+            
+            sum_line = "\t".join(split[0:2]) + "\t" + str(sum_count) + "\n"
+            sumfile.write(sum_line);
